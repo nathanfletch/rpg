8ad15e593f45cd69ccceae0a9308063cdb2cdba8
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-01 10:45:02 -0700

    Add levelUp with test

--- __tests__/hero.test.js
+++ __tests__/hero.test.js
@@ -4,27 +4,36 @@
 //     exp 100 (later)
 //     level +1
 //     hasSpecial = false;
-    
+
 //   methods
 //     levelUp - lvl++ str++ def++ if(this.lvl === 3) hasSpecial = true;
 //     defend
 
-import Hero from './../src/hero.js';
+import Hero from "./../src/hero.js";
 
-describe('Hero', () => {
+describe("Hero", () => {
+  let newHero;
+  beforeEach(() => {
+    newHero = new Hero(5, 2, 1);
+  });
 
-  test('should correctly create a hero object by calling the parent constructor', () => {
-    let newHero = new Hero(5,2,1); 
+  test("should correctly create a hero object by calling the parent constructor", () => {
     expect(newHero.hp).toEqual(5);
     expect(newHero.ap).toEqual(2);
     expect(newHero.dp).toEqual(1);
-  })
-  
-  test('should correctly create a hero object by calling the parent constructor and ', () => {
-    let newHero = new Hero(5,2,1); 
+  });
+
+  test("should correctly create a hero object by calling the parent constructor and ", () => {
     expect(newHero.level).toEqual(1);
     expect(newHero.gold).toEqual(10);
     expect(newHero.inv).toEqual({});
-
+  });
+  
+  test('should increment the hp, ap, level and dp properties by one', () => {
+    newHero.levelUp();
+    expect(newHero.level).toEqual(2);
+    expect(newHero.hp).toEqual(6);
+    expect(newHero.ap).toEqual(3);
+    expect(newHero.dp).toEqual(2);
   })
-})
+});
