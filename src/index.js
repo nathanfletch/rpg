fcc83d6b829a0d2eecc42f8014efbb872eda4436
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-01 14:12:09 -0700

    Add displayStats ui function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Unit from './../src/unit.js'
+import Hero from './../src/hero.js'
+// import Combat from './../src/combat.js'
 
 function displayStats(hero, enemy) {
   //make html 
@@ -11,25 +14,39 @@ function displayStats(hero, enemy) {
             <li>Attack: ${hero.ap}</li>
             <li>Defense: ${hero.dp}</li>
           </ul>`;
+          
+  const enemyHtml = `<h2>${enemy.name}</h2>
+          <ul>
+            <li>Health: ${enemy.hp}</li>
+            <li>Attack: ${enemy.ap}</li>
+            <li>Defense: ${enemy.dp}</li>
+          </ul>`;
   //append it
-  // $("#")
+  $("#hero-stats-display").append(heroHtml);
+  $("#enemy-stats-display").append(enemyHtml);
+}
+
+function displayIntent(enemy) {
+  // const intentHtml = ``
 }
 
 $(document).ready(function() {
   //globals
-  let myPlayer = new Hero(10, 3, 1);
+  let myHero = new Hero("ASCCI WARRIOR", 10, 3, 1);
 
   //start combat
+  //call constructor
+  let myEnemy = new Unit("Slime", 5, 2, 1);
   // displayEnemy()
-  displayStats()
-  displayIntent()
+  displayStats(myHero, myEnemy);
+  // displayIntent()
   // displayOptions()
   
   //every turn
-  displayStats()
-  displayIntent()
+  // displayStats()
+  // displayIntent()
   // displayOptions()
-  displayResults()
+  // displayResults()
 
 
 
