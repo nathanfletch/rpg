9cc6c362996862b09a0136844552c44d47152a45
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-01 10:37:44 -0700

    Add constructor unique child properties with test

--- __tests__/hero.test.js
+++ __tests__/hero.test.js
@@ -1,3 +1,14 @@
+// hero
+//   properties
+//     gold
+//     exp 100 (later)
+//     level +1
+//     hasSpecial = false;
+    
+//   methods
+//     levelUp - lvl++ str++ def++ if(this.lvl === 3) hasSpecial = true;
+//     defend
+
 import Hero from './../src/hero.js';
 
 describe('Hero', () => {
@@ -7,5 +18,13 @@ describe('Hero', () => {
     expect(newHero.hp).toEqual(5);
     expect(newHero.ap).toEqual(2);
     expect(newHero.dp).toEqual(1);
+  })
+  
+  test('should correctly create a hero object by calling the parent constructor and ', () => {
+    let newHero = new Hero(5,2,1); 
+    expect(newHero.level).toEqual(1);
+    expect(newHero.gold).toEqual(10);
+    expect(newHero.inv).toEqual({});
+
   })
 })
